a98ab074c13f6243e85c62b5205607a40e481388
Uditya <workforuditya@gmail.com>
2023-11-29 03:33:39 +0530

    Feat: Added Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from './components/Header/page'
+import Footer from './components/Footer/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" >
+     
+
+      <body className={inter.className}>
+        <main className='max-w-3xl mx-auto'>
+          
+        
+          <Header/>          
+          {children}
+          <Footer/>
+        </main>
+      </body>
+      
+    
     </html>
   )
 }
